3d3d9537b66ea37f121096440715e0912d3f2569
Dandiflower <>
2018-04-26 09:55:26 -0700

    Javascript fundamentals! onward march

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,18 @@
 // VARIABLES
 // ===========================================================
 
-var Alphabet = ["a", "b"];
+var alphabet = ["a", "b"];
 
-var Wins = 0;
+var wins = 0;
 
-var Lost = 0;
+var lost = 0;
 
 var numGuessed = 0;
 
 var numLeft = 10;
 
-
+var computerLetter;
 // computer randomly chooses
-
-
 
 
 // FUNCTIONS
@@ -32,49 +30,57 @@ var numLeft = 10;
 
 // Function that updates the wins...
 function updateWins() {
-    document.getElementById("Wins").innerHTML = "Wins: " + Wins;
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
 }
 
 // Function that updates the losses...
 function updateLost() {
-    document.getElementById("Lost").innerHTML = "Losses: " + Lost;
+    document.getElementById("lost").innerHTML = "Losses: " + lost;
 }
 
+function generateComputerLetter() {
+    computerLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log("Computer " + computerLetter)
+
+}
+generateComputerLetter()
 
 
 // MAIN PROCESS
 // ==========================================================
 
-document.getElementById("Wins").innerHTML = winnings();
+
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
     // Determines which key was pressed.
     var userGuess = event.key;
     userGuess = userGuess.toLowerCase();
-    console.log("User Guess" + userGuess)
+  
+    console.log("User Guess " + userGuess)
     // Computer Randomly chooses
-    var Computer = Alphabet[Math.floor(Math.random() * Alphabet.length)];
-    console.log("Computer" + Computer)
-   
     
+    numLeft--
     // Alerts the user the user pressed (userGuess).
-    alert("User guess: " + userGuess);
+    //alert("User guess: " + userGuess);
 
     // Alerts the computer's guess.
-    alert("computer guess: " + Computer);
+    //alert("computer guess: " + Computer);
     
-    
-    for (var i = 0; i < 11; i++ ) {
-        console.log(userGuess[i]);
 
-        if (Computer === userGuess) {
-            Wins++
-            console.log("Wins", Wins)
-        } else if (co)
-            Lost--;
-            console.log("Lost", Lost);
+
+        if (computerLetter === userGuess) {
+            wins++
+            console.log("Wins", wins)
+            numLeft = 10;
             updateWins();
-            updateLost();
+        } else if (computerLetter !== userGuess) {
+            numLeft--
         }
+
+            // Lost--;
+            // console.log("Lost", Lost);
+            // updateWins();
+            // updateLost();
+        
     };
